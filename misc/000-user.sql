d299e8614f9403e0bb6d4081c7f421b9cd91a007
ben <benjamin.james98@gmail.com>
2020-02-12 11:45:16 +1100

    Initial SQL DDL 2
    - user table data

--- misc/000-user.sql
+++ misc/000-user.sql
@@ -7,3 +7,5 @@ create table user
 	email varchar(128) not null
 );
 
+INSERT INTO user (name, password, permissions, email) VALUES ('benjamin james', '$2y$12$jN3lA7A6lwbByKkScjqb5OoHwxjA.D.y2lhAsN0.7VndHvcLerwMm', 'regional', 'benjamin.james98@gmail.com');
+INSERT INTO user (name, password, permissions, email) VALUES ('kiran walker', '$2y$12$9.NzEJq7dTGADHsM172iNun0Y1Z6vJ8mAlVL5dqqOfA9BllpeuYum', 'elder', 'kirangwalker@gmail.com');
